83f3aa9bac47925096ada2845867d92deac9b36a
Crystal Aguilar <crystal_0688@yahoo.com>
2022-09-16 13:35:18 -0700

    added functin for green/red

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+const past = document.getElementsByClassName('past');
+const future = document.getElementsByClassName('future');
+
 var dailyTimer = moment().format('LLLL');
 $('#currentDay').text(dailyTimer);
 
 for (var i=0; i < dailyTimer; i++) {
     dailyTimer++;
+    if (dailyTimer <= now) {
+        return past
+    } else if (dailyTimer => now) {
+        return future
+    }
 }
 
-moment().isBefore(dailyTimer) = $(".past")
+//moment().isBefore(dailyTimer) = $(".past")
 
 // want to locally save event in calendar
-function saveEvent(itemToDo) {
+const saveBtn = document.getElementById("saveBtn");
+
+saveBtn.addEventListener('click', (itemToDo) => {
+
     var itemToDo = JSON.parse(localStorage.getItem('textArea'));
     itemToDo.push('textArea')
     localStorage.setItem("textArea", textArea)
-}
+})
+
